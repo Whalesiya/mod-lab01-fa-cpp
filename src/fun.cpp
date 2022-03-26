8ce2f11cd0f8bc2e2090adeee9ee11036483f7ac
Whalesiya <100674297+Whalesiya@users.noreply.github.com>
2022-03-26 23:24:23 +0300

    Update fun.cpp

--- src/fun.cpp
+++ src/fun.cpp
@@ -6,61 +6,61 @@
 
 unsigned int faStr1(const char* str) {
 int inw = 0;
-int iterator = 0;
+int i = 0;
 int count = 0;
-while (*(str + iterator)) {
-if (*(str + iterator) != ' '
-&& !isdigit(*(str + iterator)) && !finWord(inw)) {
+while (*(str + i)) {
+if (!inword(inw) && !isdigit(*(str + i))
+&& *(str + i) != ' ') {
 count = count + 1;
 inw = 1;}
-else if (*(str + iterator) == ' '
-&& finWord(inw)) inw = 0;
-else if (isdigit(*(str + iterator))
-&& inw == 0) inw = 2;
+else if (inword(inw)
+&& *(str + i) == ' ') inw = 0;
+else if (inw == 0
+&& isdigit(*(str + i))) inw = 2;
 else
-if (isdigit(*(str + iterator)) && inw == 1) {
+if (inw == 1 && isdigit(*(str + i))) {
 inw = 2;
 count = count - 1;}
-iterator = iterator + 1;
+i = i + 1;
 }
 return count;
 }
 unsigned int faStr2(const char* str) {
 int count = 0;
 int inw = 0;
-int iterator = 0;
-while (*(str + iterator)) {
-if (isupper(*(str + iterator)) && inw == 0) {
+int i = 0;
+while (*(str + i)) {
+if (inw == 0 && isupper(*(str + i))) {
 count++;
 inw = 1;}
-else if (!mylower(*(str + iterator))
-&& !finWord(inw)) inw = 2;
-else if (*(str + iterator) == ' '
-&& finWord(inw)) inw = 0;
+else if (!inword(inw)
+&& !mylower(*(str + i))) inw = 2;
+else if (inword(inw)
+&& *(str + i) == ' ') inw = 0;
 else
-if (!mylower(*(str + iterator)) && inw == 1) {
+if (inw == 1 && !mylower(*(str + i))) {
 inw = 2;
 count = count - 1;}
-iterator = iterator + 1;
+i = i + 1;
 }
 return count;
 }
 unsigned int faStr3(const char* str) {
 int count = 0;
-int sum = 0;
+int k = 0;
 bool inw = false;
-int iterator = 0;
-while (*(str + iterator)) {
-if (*(str + iterator) != ' ' && inw == false) {
+int i = 0;
+while (*(str + i)) {
+if (inw == false && *(str + i) != ' ') {
 count = count + 1;
 inw = true;
-} else if (*(str + iterator) == ' ' && inw == true) {
+} else if (inw == true && *(str + i) == ' ') {
 inw = false;}
 if (inw == true)
-sum = sum + 1;
-iterator = iterator + 1;
+k = k + 1;
+i = i + 1;
 }
-count = round(static_cast<double>(sum) / static_cast<double>(count));
+count = round(static_cast<double>(k) / static_cast<double>(count));
 return count;
 }
 int inword(int inw) {
